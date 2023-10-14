8e6d39771bf3257d376dfce11ba1b633415dc10e
ANUJ KUMAR RAI <111315232+Anuj2402@users.noreply.github.com>
2023-10-14 14:03:21 +0000

    Todo web App used context Api and Local storage

---
+++ 10_TODO_Context_n_localStorage/src/contexts/TodoContext.js
@@ -0,0 +1,23 @@
+import {createContext, useContext} from "react"
+
+export const TodoContext = createContext({
+    todos: [
+        {
+            id: 1,
+            todo: " Todo msg",
+            completed: false,
+        }
+    ],
+    //functionality 
+    addTodo: (todo) => {},
+    updateTodo: (id, todo) => {},
+    deleteTodo: (id) => {},
+    toggleComplete: (id) => {}
+})
+
+
+export const useTodo = () => {
+    return useContext(TodoContext) // giving it a todo context because useContext always needs a context
+}
+
+export const TodoProvider = TodoContext.Provider 
